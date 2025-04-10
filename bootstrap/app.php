7c26f8963f49855f4f328ad6a9da53e071a353ff
Nitin Viras <nitinboricha91@gmail.com>
2025-04-10 15:05:22 +0530

    Update

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -6,32 +6,45 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware) {
+    ->withMiddleware(function (Middleware $middleware)
+    {
+
+        $middleware->group('web', [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+            \App\Http\Middleware\Localization::class
+        ]);
+
+
         $middleware->alias([
-            'auth'=>'Illuminate\Auth\Middleware\Authenticate',
-            'auth.basic'=>'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
-            'auth.session'=>'Illuminate\Session\Middleware\AuthenticateSession',
-            'cache.headers'=>'Illuminate\Http\Middleware\SetCacheHeaders',
-            'can'=>'Illuminate\Auth\Middleware\Authorize',
-            'guest'=>'Illuminate\Auth\Middleware\RedirectIfAuthenticated',
-            'password.confirm'=>'Illuminate\Auth\Middleware\RequirePassword',
-            'precognitive'=>'Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests',
-            'signed'=>'Illuminate\Routing\Middleware\ValidateSignature',
-            'verified'=>'Illuminate\Auth\Middleware\EnsureEmailIsVerified',
+            'auth' => 'Illuminate\Auth\Middleware\Authenticate',
+            'auth.basic' => 'Illuminate\Auth\Middleware\AuthenticateWithBasicAuth',
+            'auth.session' => 'Illuminate\Session\Middleware\AuthenticateSession',
+            'cache.headers' => 'Illuminate\Http\Middleware\SetCacheHeaders',
+            'can' => 'Illuminate\Auth\Middleware\Authorize',
+            'guest' => 'Illuminate\Auth\Middleware\RedirectIfAuthenticated',
+            'password.confirm' => 'Illuminate\Auth\Middleware\RequirePassword',
+            'precognitive' => 'Illuminate\Foundation\Http\Middleware\HandlePrecognitiveRequests',
+            'signed' => 'Illuminate\Routing\Middleware\ValidateSignature',
+            'verified' => 'Illuminate\Auth\Middleware\EnsureEmailIsVerified',
             'default_product_exists' => \App\Http\Middleware\CheckDefaultProductExist::class,
             'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
             'role' => \Spatie\Permission\Middleware\RoleMiddleware::class,
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'vendor_settings' => \App\Http\Middleware\VendorSettingMiddleware::class,
-            'front_setting_middleware' => \App\Http\Middleware\FrontMiddleware::class,
-            'checksubdomain' => \App\Http\Middleware\CheckSubdomain::class,
+            'front_setting_middleware' => \App\Http\Middleware\FrontMiddleware::class
         ]);
     })
-    ->withExceptions(function (Exceptions $exceptions) {
+    ->withExceptions(function (Exceptions $exceptions)
+    {
         //
     })->create();
