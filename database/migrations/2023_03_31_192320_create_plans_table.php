13ef73ec6040c63b02222d4d35a3b2ed0404a3cc
Nitin Viras <nitinboricha91@gmail.com>
2025-04-22 18:17:27 +0530

    Update

--- database/migrations/2023_03_31_192320_create_plans_table.php
+++ database/migrations/2023_03_31_192320_create_plans_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -13,25 +12,30 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('plans', function (Blueprint $table) {
+        Schema::create('plans', function (Blueprint $table)
+        {
             $table->uuid('plan_id')->primary();
-            $table->string('title',190);
-            $table->string('lang_plan_title',190)->nullable();
+            $table->string('title', 190);
+            $table->longText('description')->nullable();
+            $table->longText('lang_plan_title')->nullable();
+            $table->longText('lang_plan_description')->nullable();
             $table->double('amount')->default(0);
-            $table->enum('type',['onetime','monthly','weekly','yearly','trial','free','day']);
+            $table->enum('type', ['onetime', 'monthly', 'weekly', 'yearly', 'trial', 'free', 'day']);
             $table->integer('branch_limit')->default(0);
-            $table->enum('branch_unlimited',['yes','no'])->default('no');
+            $table->enum('branch_unlimited', ['yes', 'no'])->default('no');
 
             $table->integer('member_limit')->default(0);
-            $table->enum('member_unlimited',['yes','no'])->default('no');
+            $table->enum('member_unlimited', ['yes', 'no'])->default('no');
 
             $table->integer('staff_limit')->default(0);
-            $table->enum('staff_unlimited',['yes','no'])->default('no');
-            $table->enum('status',['active','inactive'])->default('active');
+            $table->enum('staff_unlimited', ['yes', 'no'])->default('no');
+            $table->enum('status', ['active', 'inactive'])->default('active');
 
-            $table->string('stripe_branch_id',150)->nullable();
-            $table->string('paypal_branch_id',150)->nullable();
-            $table->string('paypal_plan_id',150)->nullable();
+            $table->string('stripe_plan_id', 100)->nullable();
+            $table->string('paypal_plan_id', 100)->nullable();
+            $table->string('razorpay_plan_id', 100)->nullable();
+            $table->string('paystack_plan_id', 100)->nullable();
+            $table->string('midtrans_plan_id', 100)->nullable();
             $table->timestamps();
         });
     }
