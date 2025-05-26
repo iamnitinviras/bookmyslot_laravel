69360467c70d6bfd2ac623df350d69562a7b100d
Nitin Viras <nitinboricha91@gmail.com>
2025-05-26 12:09:25 +0530

    Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Routing\Controllers\Middleware;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -27,6 +28,15 @@ class LoginController extends Controller
         return [
             'guest' => ['except' => ['logout']],
             'auth' => ['only' => ['logout']],
+        ];
+    }
+
+    protected function credentials(Request $request)
+    {
+        return [
+            'email' => $request->{$this->username()},
+            'password' => $request->password,
+            'status' => 'active',
         ];
     }
 }
