3a94c79158c473774941062572f2b177ee2d4f49
Nitin Viras <nitinboricha91@gmail.com>
2025-05-30 00:05:45 +0530

    Update

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Routing\Controllers\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -38,5 +41,36 @@ class LoginController extends Controller
             'password' => $request->password,
             'status' => 'active',
         ];
+    }
+
+
+    public function redirectToProvider($provider)
+    {
+        return Socialite::driver($provider)->scopes(['email'])->redirect();
+    }
+
+    public function handleProviderCallback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+        $authUser = $this->CreateUser($user, $provider);
+        Auth::login($authUser, true);
+        return redirect('/');
+    }
+
+    public function CreateUser($user, $provider)
+    {
+        $authUser = User::where('provider_id', $user->id)->where('drop_status', '=', 'no')->first();
+        if ($authUser) {
+            return $authUser;
+        }
+
+        return User::create([
+            'name' => $user->name,
+            'email' => $user->email,
+            'user_type' => 'customer',
+            'verified' => 1,
+            'provider' => $provider,
+            'provider_id' => $user->id
+        ]);
     }
 }
